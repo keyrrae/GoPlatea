ce1e05ace4b001d864f27c3ef402ac7fd11198d3
keyrrae <xuan-wang@outlook.com>
2017-05-18 23:57:01 -0700

    integrating performance measurements into the web app, WIP

---
+++ HackPlatea/envhelper.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"os"
+	"log"
+	"os/exec"
+)
+
+type EnvHelper struct{
+	Filename []string
+}
+
+func NewEnvHelper(filename []string) EnvHelper {
+	res := EnvHelper{
+		Filename: filename,
+	}
+
+	return res
+}
+
+func (eh EnvHelper) GetCurrDirectory() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dir + "/"
+}
+
+func (eh EnvHelper) ClearWorkspace() (string, error) {
+	for _, file := range eh.Filename {
+		os.Remove(file)
+	}
+	cmd := exec.Command("touch", ".hhconfig")
+	output, err := cmd.CombinedOutput()
+	res := string(output)
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
+
+func (eh EnvHelper) WriteProgToSystem(prog string) error {
+	f, err := os.Create(eh.Filename[0])
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write([]byte(prog))
+	if err != nil {
+		return err
+	}
+
+	f.Sync()
+	return nil
+}
---
+++ HackPlatea/test.php
@@ -0,0 +1,3 @@
+<?hh
+
+echo "nononffo";
